2e63fa6d327e674b7cfd6a60d64ad2951440597c
matteobelin <matteobelin85@gmail.com>
2026-01-31 14:01:22 +0100

    Initialisation du projet Spring et Mise en place de la CI

---
+++ src/main/java/com/togezzer/chat_sauvegarde/ChatSauvegardeApplication.java
@@ -0,0 +1,13 @@
+package com.togezzer.chat_sauvegarde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatSauvegardeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatSauvegardeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/togezzer/chat_sauvegarde/ChatSauvegardeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.togezzer.chat_sauvegarde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatSauvegardeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
