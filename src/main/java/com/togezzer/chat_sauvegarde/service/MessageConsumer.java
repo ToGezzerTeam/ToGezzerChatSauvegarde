d4c99dce1cea88892de4a94fcdd977a4f287dfe0
matteobelin <matteobelin85@gmail.com>
2026-02-01 21:24:30 +0100

    retour relecture

--- src/main/java/com/togezzer/chat_sauvegarde/service/MessageConsumer.java
+++ src/main/java/com/togezzer/chat_sauvegarde/service/MessageConsumer.java
@@ -1,16 +1,18 @@
 package com.togezzer.chat_sauvegarde.service;
 
 import com.togezzer.chat_sauvegarde.dto.MessageDTO;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.dao.DataAccessException;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class MessageConsumer {
 
@@ -23,9 +25,11 @@ public class MessageConsumer {
         try {
             messageService.saveMessage(message);
             log.info("Message sauvegardé et retiré de la queue.");
-        } catch (Exception e) {
+        } catch (DataAccessException e) {
             log.error("Erreur Mongo lors de la sauvegarde : {}", e.getMessage());
             throw e;
+        } catch (ConstraintViolationException e) {
+            log.error("Données invalides : {}", e.getMessage());
         }
     }
 }
