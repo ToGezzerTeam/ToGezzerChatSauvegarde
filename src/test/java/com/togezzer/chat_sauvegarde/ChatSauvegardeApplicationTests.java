04442cd544bcef452b93fb480c2211154706366c
matteobelin <matteobelin85@gmail.com>
2026-02-01 17:35:54 +0100

    Modification des properties

--- src/test/java/com/togezzer/chat_sauvegarde/ChatSauvegardeApplicationTests.java
+++ src/test/java/com/togezzer/chat_sauvegarde/ChatSauvegardeApplicationTests.java
@@ -2,8 +2,12 @@ package com.togezzer.chat_sauvegarde;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ChatSauvegardeApplicationTests {
 
 	@Test
