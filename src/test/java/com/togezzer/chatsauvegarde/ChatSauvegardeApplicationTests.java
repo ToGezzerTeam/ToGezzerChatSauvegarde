e25b5222f4e169337d6b933bf4243f5ba5cac2a1
matteobelin <matteobelin85@gmail.com>
2026-02-07 16:12:23 +0100

    Ajout d'un assert pour sonarQube

--- src/test/java/com/togezzer/chatsauvegarde/ChatSauvegardeApplicationTests.java
+++ src/test/java/com/togezzer/chatsauvegarde/ChatSauvegardeApplicationTests.java
@@ -1,17 +1,24 @@
 package com.togezzer.chatsauvegarde;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @TestPropertySource(locations = "classpath:application-test.properties")
 class ChatSauvegardeApplicationTests {
 
+	@Autowired
+	private ChatSauvegardeApplication applicationContext;
+
 	@Test
 	void contextLoads() {
+		assertThat(applicationContext).isNotNull();
 	}
 
 }
